d19f76c298668b1e165ea7f7dd1aded0a9f050aa
RohitK <119722685+RohitKotha@users.noreply.github.com>
2024-02-04 16:44:10 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Permutations of a given string/permutations-of-a-given-string.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    vector<string>v1;
+    void per(string s,int start,int end){
+    if (start >= end){ 
+        v1.push_back(s);
+        return ;
+        }
+        
+    for (int i = start ; i<=end ; i++){
+        
+        swap(s[start],s[i]);
+        
+        per(s,start+1,end);
+        
+        swap(s[start],s[i]);
+        
+    }
+}
+	public:
+		vector<string>find_permutation(string s)
+		{
+		    
+            int co=0;
+            per(s,0,s.size()-1);
+            sort(v1.begin(),v1.end());
+            v1.erase(unique(v1.begin(), v1.end()), v1.end());
+            return v1;
+		}
+};
+
+
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin >> t;
+    while(t--)
+    {
+	    string S;
+	    cin >> S;
+	    Solution ob;
+	    vector<string> ans = ob.find_permutation(S);
+	    sort(ans.begin(),ans.end());
+	    for(auto i: ans)
+	    {
+	    	cout<<i<<" ";
+	    }
+	    cout<<"\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
