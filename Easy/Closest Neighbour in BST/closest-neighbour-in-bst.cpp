693fb2e5826afbc26a121def167fcee744e693e2
RohitK <119722685+RohitKotha@users.noreply.github.com>
2024-03-31 14:49:04 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Closest Neighbour in BST/closest-neighbour-in-bst.cpp
@@ -0,0 +1,148 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int key;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->key = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+// Function to Build Tree
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+// User function Template for C++
+/*
+//Structure of the Node of the BST is as
+struct Node {
+    int key;
+    Node* left, *right;
+};
+
+
+   'N' is the element
+   'size' is the total element in BST
+  */
+
+class Solution {
+  public:
+    int findMaxForN(Node* root, int n) {
+        // code here
+        int ans=0;
+        queue<Node*>q1;
+        q1.push(root);
+        
+        
+        while(!q1.empty()){
+           int s1=q1.size();
+           while(s1!=0){
+               Node*temp1=q1.front();
+               q1.pop();
+               if(temp1->left!=NULL){
+                   q1.push(temp1->left);
+               }
+               if(temp1->right!=NULL){
+                   q1.push(temp1->right);
+               }
+               if(temp1->key<=n and temp1->key>ans){
+                   ans=temp1->key;
+               }
+               s1--;
+           }
+        }
+        return ans==0?-1:ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+        string s;
+        getline(cin >> ws, s);
+        int x;
+        cin >> x;
+
+        Node* root = buildTree(s);
+        Solution ob;
+        cout << ob.findMaxForN(root, x) << endl;
+    }
+    return 1;
+}
+
+// } Driver Code Ends
