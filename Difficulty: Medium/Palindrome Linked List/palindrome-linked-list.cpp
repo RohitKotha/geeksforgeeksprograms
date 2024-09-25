b747321a20196577f6e27dfa97f173a99ebeb7e7
RohitK <119722685+RohitKotha@users.noreply.github.com>
2024-09-25 14:40:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Palindrome Linked List/palindrome-linked-list.cpp
@@ -0,0 +1,160 @@
+//{ Driver Code Starts
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <cmath>
+#include <cstdio>
+#include <ios>
+#include <iostream>
+#include <random>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+class Solution {
+  public:
+    // Function to check whether the list is palindrome.
+    bool isPalindrome(Node *head) {
+        // Your code here
+        Node*i=head;
+        long long ok=0;
+        long long n=0;
+        Node*temp=head;
+        while(temp){
+            temp=temp->next;
+            n++;
+        }
+        if(n==1){
+            return true;
+        }
+        if(n==2){
+            if(head->data==head->next->data){
+                return true;
+            }
+            return false;
+        }
+        if(n%2==0){
+            ok=(n/2);
+        }else{
+            ok=(n/2)+1;
+        }
+        Node*j=head;
+        long long co=0;
+        while(co<ok){
+            j=j->next;
+            co++;
+        }
+
+
+
+         Node *curr = j, *prev = nullptr, *next;
+
+      // Traverse all the nodes of Linked List
+    while (curr != nullptr) {
+      
+        // Store next
+        next = curr->next;
+      
+        // Reverse current node's next pointer
+        curr->next = prev;
+      
+        // Move pointers one position ahead
+        prev = curr;
+        curr = next;
+    }
+
+
+j=prev;
+
+        while(i!=NULL and j!=NULL){
+            //cout<<i->data<<".."<<j->data;
+            //cout<<endl;
+            if(i->data!=j->data){
+                return false;
+            }
+            i=i->next;
+            j=j->next;
+        }
+        return true;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+/* Function to print nodes in a given linked list */
+void printList(struct Node *head) {
+    struct Node *temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        if (arr.empty()) {
+            cout << "empty" << endl;
+            continue;
+        }
+
+        struct Node *head = new Node(arr[0]);
+        struct Node *tail = head;
+        for (int i = 1; i < arr.size(); ++i) {
+            tail->next = new Node(arr[i]);
+            tail = tail->next;
+        }
+        Solution ob;
+        if (ob.isPalindrome(head))
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+
+        // Clean up the remaining nodes to avoid memory leaks
+        while (head != NULL) {
+            struct Node *temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+    return 0;
+}
+
+// } Driver Code Ends
