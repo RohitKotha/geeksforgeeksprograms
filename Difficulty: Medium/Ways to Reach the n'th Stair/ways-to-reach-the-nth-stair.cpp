78309a15b78f4bf9d26d7accd8f1342eef80001c
RohitK <119722685+RohitKotha@users.noreply.github.com>
2025-03-11 10:03:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Ways to Reach the n'th Stair/ways-to-reach-the-nth-stair.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int countWays(int n) {
+        // your code here
+        if(n==0 or n==1){
+            return n;
+        }
+        int one=1,two=1;
+        for(int i=0;i<n-1;i++){
+            int temp=one;
+            one =one+two;
+            two=temp;
+        }
+        return one;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main() {
+    // taking total testcases
+    int t;
+    cin >> t;
+    while (t--) {
+        // taking stair count
+        int m;
+        cin >> m;
+        Solution ob;
+        cout << ob.countWays(m) << endl; // Print the output from our pre computed array
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
