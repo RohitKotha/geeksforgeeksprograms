7eb63c50f4582a450717755a522469902d8e7322
RohitK <119722685+RohitKotha@users.noreply.github.com>
2025-04-01 17:38:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find the number of islands/find-the-number-of-islands.cpp
+++ Difficulty: Medium/Find the number of islands/find-the-number-of-islands.cpp
@@ -5,38 +5,47 @@ using namespace std;
 // } Driver Code Ends
 
 class Solution {
-  public:
-    // Function to find the number of islands.
-    int n,m;
-    vector<vector<int>> dir{{1,0},{-1,0},{0,-1},{0,1},{1,1},{-1,-1},{1,-1},{-1,1}};
-    void solve(vector<vector<char>>& grid,int i,int j){
-        //base case
-        if(i<0 || j<0 || i>=n || j>=m || grid[i][j]=='0') return;
-        grid[i][j]='0';
-        for(auto &it:dir){
-            int newi=i+it[0];
-            int newj=j+it[1];
-            solve(grid,newi,newj);
-        }
-        
-    }
+public:
+void bfs(int row,int col, vector<vector<int>>&vis,vector<vector<char>>&grid){
+      vis[row][col]=1;
+      queue<pair<int,int>>q1;
+      q1.push({row,col});
+      int n=grid.size();
+      int m=grid[0].size();
+      while(!q1.empty()){
+          int r=q1.front().first;
+          int c=q1.front().second;
+          q1.pop();
+          for(int delrow=-1;delrow<=1;delrow++){
+              for(int delcol=-1;delcol<=1;delcol++){
+                  int nrow=r+delrow;
+                  int ncol=c+delcol;
+                  if(nrow>=0 and nrow<n and ncol>=0 and ncol<m and grid[nrow][ncol]=='1' and !vis[nrow][ncol]){
+                     vis[nrow][ncol]=1;
+                      q1.push({nrow,ncol});
+                     
+                  }
+              }
+          }
+      }
+  }
     int numIslands(vector<vector<char>>& grid) {
-        // CodeGenius
-        n=grid.size();
-        m=grid[0].size();
-        int count=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]=='1'){
-                    solve(grid,i,j);
-                    count++;
+          // code here
+           int n=grid.size();
+      int m=grid[0].size();
+        int ans=0;
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        for(int row=0;row<n;row++){
+            for(int col=0;col<m;col++){
+                if(!vis[row][col]&& grid[row][col] == '1'){
+                 ans++;
+                 bfs(row,col,vis,grid);
                 }
             }
         }
-        return count;
+        return ans;
     }
 };
-
 
 //{ Driver Code Starts.
 int main() {
@@ -54,7 +63,9 @@ int main() {
         Solution obj;
         int ans = obj.numIslands(grid);
         cout << ans << '\n';
-    }
+    
+cout << "~" << "\n";
+}
     return 0;
 }
 // } Driver Code Ends
